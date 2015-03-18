57f91dfc19ddd1cdd00e0d3e1ee55451e53a5cbe
Nghia Pham <nghia.pham@riverorchid.com>
2015-03-19 01:26:53 +0700

    Static HTML,CSS for Home, without Animations

--- Lesson_01_Build1/html/html/js/script.js
+++ Lesson_01_Build1/html/html/js/script.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-soundManager.setup({
-    url: 'js/swf/',
-    onready: function() {
-      var mySound = soundManager.createSound({
-        id: 'introLesson1',
-        url: 'js/mp3/lesson1/intro.mp3'
-      });
-//      mySound.play();
-      console.log(mySound.length);
-      var count = 0;
-      mySound.play({
-        multiShot: false,
-        whileplaying: function() {
-          var currentTime = this.position;
-          var duration = this.duration;
-//          console.log (currentTime);
-          // demo only: show sound position while playing, for context
-          $('.range').val(currentTime/duration*100);
-          if (currentTime > 300 && count == 0 ){
-            $('#sentence1').fadeIn(1000);
-            count++;
-          }
-//          console.log(this.position/1000);
+    soundManager.setup({
+        url: 'js/swf/',
+        onready: function () {
+            var mySound = soundManager.createSound({
+                id: 'introLesson1',
+                url: 'js/mp3/lesson1/intro.mp3'
+            });
+
+            console.log(mySound.length);
+            var count = 0;
+
+            /*mySound.play({
+                multiShot: false,
+                whileplaying: function () {
+                    var currentTime = this.position;
+                    var duration = this.duration;
+                    $('.range').val(currentTime / duration * 100);
+                    if (currentTime > 300 && count == 0) {
+                        $('#sentence1').fadeIn(1000);
+                        count++;
+                    }
+                }
+            });*/
+
+        },
+        ontimeout: function () {
+            // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
         }
-      });
-    },
-    ontimeout: function() {
-      // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
-    }
-  });
+    });
+
 
 })
 ;
